59773d54c8809bd2c9d6d1c1d39b2a56b4d1d004
Graeme Lockley <graemel@no9.co.za>
2016-03-31 06:39:29 +0100

    Made service noun singular to be consistent with the User Service

--- domain/src/main/java/za/co/no9/app/service/AccountService.java
+++ domain/src/main/java/za/co/no9/app/service/AccountService.java
@@ -7,28 +7,28 @@ import za.co.no9.app.util.Either;
 import java.util.Optional;
 import java.util.stream.Stream;
 
-public class AccountsService {
-    public Either<AccountsServiceFailures, Stream<Account>> view(UserName userName) {
+public class AccountService {
+    public Either<AccountServiceFailures, Stream<Account>> view(UserName userName) {
         final Optional<User> user = DI.get(Repository.class).findUser(userName);
 
         if (user.isPresent()) {
             return Either.right(user.get().accounts());
         } else {
-            return Either.left(AccountsServiceFailures.UNKNOWN_USER);
+            return Either.left(AccountServiceFailures.UNKNOWN_USER);
         }
     }
 
-    public Either<AccountsServiceFailures, Stream<Transaction>> accountTransactions(AccountRef accountRef) {
+    public Either<AccountServiceFailures, Stream<Transaction>> accountTransactions(AccountRef accountRef) {
         final Optional<Account> account = DI.get(Repository.class).findAccount(accountRef);
 
         if (account.isPresent()) {
             return Either.right(account.get().transactions());
         } else {
-            return Either.left(AccountsServiceFailures.UNKNOWN_ACCOUNT);
+            return Either.left(AccountServiceFailures.UNKNOWN_ACCOUNT);
         }
     }
 
-    public enum AccountsServiceFailures {
+    public enum AccountServiceFailures {
         UNKNOWN_ACCOUNT, UNKNOWN_USER
     }
 }
--- domain/src/test/java/za/co/no9/app/service/AccountServiceTest.java
+++ domain/src/test/java/za/co/no9/app/service/AccountServiceTest.java
@@ -3,7 +3,7 @@ package za.co.no9.app.service;
 import org.junit.Before;
 import org.junit.Test;
 import za.co.no9.app.domain.*;
-import za.co.no9.app.service.AccountsService.AccountsServiceFailures;
+import za.co.no9.app.service.AccountService.AccountServiceFailures;
 import za.co.no9.app.util.DI;
 import za.co.no9.app.util.Either;
 
@@ -14,7 +14,7 @@ import java.util.stream.Stream;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-public class AccountsServiceTest {
+public class AccountServiceTest {
     public static final UserName USER_A = UserName.from("graeme");
     public static final UserName USER_B = UserName.from("mary-anne");
     public static final UserName UNKNOWN_USER = UserName.from("joe-joe");
@@ -35,12 +35,12 @@ public class AccountsServiceTest {
                         Transaction.from(Date.from(Instant.parse("2016-01-01T10:15:30.00Z")), TransactionRef.from("Transaction A"), TransactionDescription.from("Reference A"), Money.from(123.45)),
                         Transaction.from(Date.from(Instant.parse("2016-01-02T10:15:30.00Z")), TransactionRef.from("Transaction B"), TransactionDescription.from("Reference B"), Money.from(123.45)))
                 .build());
-        DI.register(new AccountsService());
+        DI.register(new AccountService());
     }
 
     @Test
     public void should_return_no_accounts_if_the_user_has_no_accounts() throws Exception {
-        final Either<AccountsServiceFailures, Stream<Account>> view = DI.get(AccountsService.class).view(USER_A);
+        final Either<AccountServiceFailures, Stream<Account>> view = DI.get(AccountService.class).view(USER_A);
 
         assertTrue(view.isRight());
         assertEquals(0, view.right().count());
@@ -48,7 +48,7 @@ public class AccountsServiceTest {
 
     @Test
     public void should_return_two_accounts_if_the_user_is_known() throws Exception {
-        final Either<AccountsServiceFailures, Stream<Account>> view = DI.get(AccountsService.class).view(USER_B);
+        final Either<AccountServiceFailures, Stream<Account>> view = DI.get(AccountService.class).view(USER_B);
 
         assertTrue(view.isRight());
         assertEquals(2, view.right().count());
@@ -56,23 +56,23 @@ public class AccountsServiceTest {
 
     @Test
     public void should_return_an_error_if_user_is_unknown() throws Exception {
-        final Either<AccountsServiceFailures, Stream<Account>> view = DI.get(AccountsService.class).view(UNKNOWN_USER);
+        final Either<AccountServiceFailures, Stream<Account>> view = DI.get(AccountService.class).view(UNKNOWN_USER);
 
         assertTrue(view.isLeft());
-        assertEquals(AccountsServiceFailures.UNKNOWN_USER, view.left());
+        assertEquals(AccountServiceFailures.UNKNOWN_USER, view.left());
     }
 
     @Test
     public void should_return_an_error_for_UNKNOWN_ACCOUNT() throws Exception {
-        final Either<AccountsServiceFailures, Stream<Transaction>> findResult = DI.get(AccountsService.class).accountTransactions(UNKNOWN_ACCOUNT);
+        final Either<AccountServiceFailures, Stream<Transaction>> findResult = DI.get(AccountService.class).accountTransactions(UNKNOWN_ACCOUNT);
 
         assertTrue(findResult.isLeft());
-        assertEquals(AccountsServiceFailures.UNKNOWN_ACCOUNT, findResult.left());
+        assertEquals(AccountServiceFailures.UNKNOWN_ACCOUNT, findResult.left());
     }
 
     @Test
     public void should_return_no_transactions_for_ACCOUNT_B_1() throws Exception {
-        final Either<AccountsServiceFailures, Stream<Transaction>> findResult = DI.get(AccountsService.class).accountTransactions(ACCOUNT_B_1);
+        final Either<AccountServiceFailures, Stream<Transaction>> findResult = DI.get(AccountService.class).accountTransactions(ACCOUNT_B_1);
 
         assertTrue(findResult.isRight());
         assertEquals(0, findResult.right().count());
@@ -80,7 +80,7 @@ public class AccountsServiceTest {
 
     @Test
     public void should_return_two_transactions_for_ACCOUNT_B_2() throws Exception {
-        final Either<AccountsServiceFailures, Stream<Transaction>> findResult = DI.get(AccountsService.class).accountTransactions(ACCOUNT_B_2);
+        final Either<AccountServiceFailures, Stream<Transaction>> findResult = DI.get(AccountService.class).accountTransactions(ACCOUNT_B_2);
 
         assertTrue(findResult.isRight());
         assertEquals(2, findResult.right().count());
