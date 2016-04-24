c43ee5189fa691258b89cfa62ba9089309c73e93
Graeme Lockley <graemel@no9.co.za>
2016-04-24 22:02:28 +0100

    Removed unnecessary reference to the Credential class.

--- domain/src/test/java/za/co/no9/app/aggregate/client/ClientServiceTest.java
+++ domain/src/test/java/za/co/no9/app/aggregate/client/ClientServiceTest.java
@@ -5,7 +5,6 @@ import org.junit.Test;
 import za.co.no9.app.aggregate.client.ClientService.ClientServiceFailure;
 import za.co.no9.app.domain.Password;
 import za.co.no9.app.domain.UserName;
-import za.co.no9.app.read.Credential;
 import za.co.no9.app.util.DI;
 import za.co.no9.app.util.EventStore;
 
@@ -14,7 +13,6 @@ import static org.junit.Assert.assertEquals;
 public class ClientServiceTest {
     private static final UserName VALID_CLIENT_NAME = new UserName("graeme");
     private static final Password VALID_CLIENT_PASSWORD = new Password("password");
-    private static final Credential VALID_CLIENT_CREDENTIAL = Credential.from(VALID_CLIENT_NAME, VALID_CLIENT_PASSWORD);
 
     private ClientService clientService = new ClientService();
     private EventStore eventStore = new EventStore();
@@ -27,11 +25,11 @@ public class ClientServiceTest {
 
         eventStore.registerEventHandler(clientService);
 
-        clientService.addClient(new AddClientCommand(VALID_CLIENT_CREDENTIAL.clientID(), VALID_CLIENT_PASSWORD));
+        clientService.addClient(new AddClientCommand(VALID_CLIENT_NAME, VALID_CLIENT_PASSWORD));
     }
 
     @Test
     public void should_throw_an_exception_when_adding_a_duplicate_client() throws Exception {
-        assertEquals(ClientServiceFailure.DUPLICATE_CLIENT_ID, clientService.addClient(new AddClientCommand(VALID_CLIENT_CREDENTIAL.clientID(), VALID_CLIENT_PASSWORD)).get());
+        assertEquals(ClientServiceFailure.DUPLICATE_CLIENT_ID, clientService.addClient(new AddClientCommand(VALID_CLIENT_NAME, VALID_CLIENT_PASSWORD)).get());
     }
 }
